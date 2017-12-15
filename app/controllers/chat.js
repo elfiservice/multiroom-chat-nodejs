e3f1ebadd161be469993b4219b4e195f2f9df8ab
elfiservice <elfiservice@hotmail.com>
2017-12-15 08:06:56 -0300

    feat: secao13-aula62-validar-acesso-chat-via-post 1- Alterado a view index.ejs   a. Add no form a action (c amiho da route) e o metodo (post) 2- Alterado a routs   a. Do chat     i. Verificando se tem apelido antes de renderizar a view chat     ii. Recebendo os erros e enviando para a view Index !!   b. Da index     i. Add o paramentro validacao vazia para a view index renderizar sem erros 3- Add na view index   a. O if testando se tem erros na variavel validacao   b. Se tiver     i. Feito o for para percorrer os eerros na variavel validação E mostrar em tela

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,17 @@
 module.exports.startChat = function(application, req, res) {
+
+    //body vem do modulo body-parser - acesso os dados da view via post no formato Json
+    var dadosForm = req.body;
+
+    req.assert('apelido', 'Preencha um apelido.').notEmpty();
+    req.assert('apelido', 'O apelido deve ter de 3 a 15 caracters.').len(3, 15);
+
+    var error = req.validationErrors();
+
+    if(error) {
+        res.render('index', {validacao: error});
+        return;
+    }
+
     res.render('chat.ejs');
 }
