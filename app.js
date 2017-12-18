f8ddcede3a3a6e954a5d9b6656cadcbd217d5a8e
elfiservice <elfiservice@hotmail.com>
2017-12-18 07:17:50 -0300

    chore: secao13-aula66-abrindo-conex-websocket

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ var serverPort = app.listen(8080, function() {
 });
 
 //inicia o websocket na mesma porta do protocolo http
-require('socket.io').listen(serverPort);
+var io = require('socket.io').listen(serverPort);
+
+//criar uma conexao do websocket
+//informando q estamos escutando Eventos de conexao
+//é executado quando uma tentativa d conexao é executado do lado do cliente (instancia do obj io)
+//socket (o parameto) é a conexao criada retornada por cb
+io.on('connection', function(socket){
+  console.log('user COnectado no Chat!');
+
+  //usa a conexao socket para criar uma escuta de um evento de deisconexao do chat ( quando a instancia do obj io nao existir mais)
+  socket.on('disconnect', function(){
+    console.log('user Desconectou do Chat');
+  });
+});
