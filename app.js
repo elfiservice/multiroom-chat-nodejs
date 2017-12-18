6733fb9b3ff2b53fe5430e8bae8f7c6083ea7eaf
elfiservice <elfiservice@hotmail.com>
2017-12-18 06:54:32 -0300

    chore: secao13-aula63-64-65-install-e-imp-websocket

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 var app = require('./config/server');
 
 // parametrizar a posta d escuta da porta 80 padrao
-app.listen(8080, function() {
+var serverPort = app.listen(8080, function() {
   console.log("servidor online!");
 });
+
+//inicia o websocket na mesma porta do protocolo http
+require('socket.io').listen(serverPort);
